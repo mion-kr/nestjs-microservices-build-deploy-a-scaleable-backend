d3f41f51abdb4b45c96c7f381e7008dd72dfba45
mion <whddbs311@gmail.com>
2023-07-25 10:36:46 +0900

    ♻️ refector code

--- libs/common/src/logger/logger.module.ts
+++ libs/common/src/logger/logger.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     PinoLoggerModule.forRoot({
       pinoHttp: {
+        genReqId: function (req, res) {
+          const existingID = req.id ?? req.headers['x-request-id'];
+          if (existingID) return existingID;
+          const id = randomUUID();
+          res.setHeader('X-Request-Id', id);
+          return id;
+        },
         serializers: {
           // 요청 내용의 Body를 로그에 남기기 위해 설정
           req: (req) => {
